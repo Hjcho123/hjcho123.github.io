f2b72c0c9ea3a2a4c3af00b4176b9feda535d48d
Hjcho123 <choheejae12306@gmail.com>
2025-10-04 17:40:13 +0000

    Deploying to gh-pages from @ Hjcho123/hjcho123.github.io@109ee91aeb0dc853d45b06406c2515635a7bea8d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -60,22 +60,7 @@ ninja.data = [{
             },},{id: "news-hi-i-just-finished-building-the-basic-framework-of-this-website-today-hoping-to-build-this-site-into-something-great",
           title: 'Hi! I just finished building the basic framework of this website today! Hoping...',
           description: "",
-          section: "News",},{id: "projects-deep-learning-2d-and-3d-gradient-descent",
-          title: 'Deep Learning: 2D and 3D Gradient Descent',
-          description: "Developing a gradient descent model using Deep Learning",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-fractal-tree-generation-in-python",
-          title: 'Fractal Tree Generation in Python',
-          description: "Methods and strategies I used for this simple Fractal Tree program I wrote in Python",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-enhancing-the-black-scholes-with-dl-based-sentiment-analysis",
-          title: 'Enhancing the Black Scholes with DL-based Sentiment Analysis',
-          description: "BRUH WHAT AM I DOING WITH MY LIFE",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{
+          section: "News",},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
